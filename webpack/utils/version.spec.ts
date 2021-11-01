7bdeeabaf950676efdb07f613116bac1297c6403
xcesiv <10799441+xcesiv@users.noreply.github.com>
2021-11-01 16:23:44 -0400

    🐛 FIX: update version spec

--- webpack/utils/version.spec.ts
+++ webpack/utils/version.spec.ts
@@ -1,11 +1,11 @@
 const { generateVersion, getPackageJsonVersion } = require('./version');
 
 it('test pre-release', () => {
-  expect(generateVersion({ version: '0.0.2-alpha.0', commitsCount: 100 })).toEqual('0.1.100');
+  expect(generateVersion({ version: '1.31.0-alpha.0', commitsCount: 100 })).toEqual('1.30.100');
 });
 
 it('test normal', () => {
-  expect(generateVersion({ version: '0.0.2', commitsCount: 100 })).toEqual('0.0.2');
+  expect(generateVersion({ version: '1.31.0', commitsCount: 100 })).toEqual('1.31.0');
 });
 
 it('test getPackageJsonVersion', () => {
